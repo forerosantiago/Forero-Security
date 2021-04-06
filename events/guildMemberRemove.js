00f3fab80e53c1cf91637eb77ff304908a943c88
Replit user <replituser@example.com>
2021-04-06 12:26:17 +0000

    fix typo in guildMemberRemove event file

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js')
 
 module.exports = {
-  name: 'guildMemberAdd',
+  name: 'guildMemberRemove',
   once: false,
   execute (member, client) {
-    const channel = client.channels.cache.get('784853103932997672')
+    const channel = client.channels.cache.get(process.env.welcome_channel_id)
     if (!channel) return
-    if (member.guild.id !== '782081352396832778') return
+    if (member.guild.id !== process.env.guild_id) return
 
     const color = '#FF4136'
 
     const embed = new Discord.MessageEmbed()
-      .setDescription(`**${member.user.tag}** has left **${member.guild.name}**, now we are the **${member.guild.memberCount}** members :pensive:`)
+      .setDescription(`**${member.user.tag}** has left **${member.guild.name}**, now we are **${member.guild.memberCount}** members :pensive:`)
       .setColor(color)
 
     channel.send(embed)
