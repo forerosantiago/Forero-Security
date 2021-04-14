f8b87ec0b955b7607a1b807fff3fcc8296f1f7e8
Replit user <replituser@example.com>
2021-04-14 12:22:03 +0000

    remove events webhooks

--- events/ready.js
+++ events/ready.js
@@ -4,19 +4,6 @@ module.exports = {
   name: 'ready',
   once: true,
   execute (client) {
-    axios.post(process.env.status_webhook, {
-      username: 'Forero Security',
-      embeds: [
-        {
-          title: ':green_circle: Started',
-          color: 4036976,
-          timestamp: new Date()
-        }
-      ]
-
-    })
-      .catch(function (error) {
-        console.log(error)
-      })
+  
   }
 }
